ee3f6d63878ec7782ff54852ff0d0df2d401b83f
Dev-Arnold <igbinakearnold@gmail.com>
2025-05-07 05:06:19 +0100

    changed the cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,9 +82,10 @@ const signin = async (req, res, next) => {
     // Set the token as an HTTP-only cookie
     res.cookie("token", token, {
       httpOnly: true, // Prevents JavaScript access for security
-      secure: process.env.NODE_ENV === "production", // Use true in production (HTTPS required)
-      // sameSite: "none", // Prevent CSRF attacks
-      sameSite: "strict", // Prevent CSRF attacks
+      // secure: process.env.NODE_ENV === "production", // Use true in production (HTTPS required)
+      secure: true, // Use true in production (HTTPS required)
+      sameSite: "None", // Prevent CSRF attacks
+      // sameSite: "strict", // Prevent CSRF attacks
        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });
 
@@ -121,9 +122,10 @@ const loginAsAdmin = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true, // Prevents JavaScript access for security
-      secure: process.env.NODE_ENV === "production", // Use true in production (HTTPS required)
-      // sameSite: "none", // Prevent CSRF attacks
-      sameSite: "strict", // Prevent CSRF attacks
+      // secure: process.env.NODE_ENV === "production", // Use true in production (HTTPS required)
+      secure: true, // Use true in production (HTTPS required)
+      sameSite: "None", // Prevent CSRF attacks
+      // sameSite: "strict", // Prevent CSRF attacks
        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });
 
